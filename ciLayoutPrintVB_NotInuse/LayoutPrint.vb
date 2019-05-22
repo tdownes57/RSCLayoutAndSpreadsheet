61c1b39bc997fe2e5fcb4455ceaea32c659205ba
CardIntegrators <programming@CardIntegrators.com>
2019-05-21 19:01:06 -0700

    Added a new Library in VB.  ciLayoutPrintLib.

---
+++ ciLayoutPrintVB_NotInuse/LayoutPrint.vb
@@ -0,0 +1,224 @@
+Option Explicit On ''Added 5/21/2019 td 
+Option Strict On ''Added 5/21/2019 td 
+''
+''Added 5/21/2019 thomas downes 
+''
+Imports System.Drawing
+
+Public Class LayoutPrint
+    ''
+    ''Added 5/21/2019 td  
+    ''
+    Public Sub GenerateBuildImage(Optional ByRef pref_image As Image = Nothing, Optional ByVal pboolLargeLandscape As Boolean = False)
+        ''
+        ''Added 5/6/2019 thomas downes  
+        ''
+        ''panelLayout
+        ''
+        ''Let's create the image we can write our text on.  
+        ''
+        ''  https://stackoverflow.com/questions/8022174/how-can-i-write-on-an-image-using-vb-net
+        ''
+        Dim img_LargeLandscape As System.Drawing.Image
+        Dim img_Rotated As Image
+        Dim imgPanelBackground As System.Drawing.Image
+        Dim imgPanelCropped As System.Drawing.Image
+        Dim gr As Graphics ''= Graphics.FromImage(img)
+        Dim CropRect As New Rectangle(0, 0, panelLayout.Width, panelLayout.Height)
+
+        ''labelDefault1.Visible = False
+        ''LabelDefault2.Visible = False
+
+        imgPanelBackground = panelLayout.BackgroundImage
+        ''imgPanelCropped = imgPanelBackground
+        imgPanelCropped = New Bitmap(CropRect.Width, CropRect.Height)
+
+        ''Crop the image to what you see in the Panel Layout.
+        ''
+        ''   (I have removed the Zoom And StretchImage from the PictureBox, 
+        ''   so part of the image might lie outside of the PanelLayout area. ---5/7/2019 td)
+        ''
+        Try
+            Using graphicsCroppping = Graphics.FromImage(imgPanelCropped)
+                graphicsCroppping.DrawImage(imgPanelBackground, New Rectangle(0, 0, CropRect.Width, CropRect.Height), CropRect, GraphicsUnit.Pixel)
+                ''This can be done when the application closes. -----imgPanelBackground.Dispose()
+                ''imgPanelCropped.Save(fileName)
+            End Using
+        Catch ex As Exception
+            ''
+            ''Added 5/9/2019 td
+            ''
+            MessageBox.Show("Resizing error, GenerateBuildImage:  " & ex.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End Try
+
+        img_LargeLandscape = imgPanelCropped
+
+        ''Encapsulated 5/7/2019 td
+        Const c_boolCallProcedureForText As Boolean = True
+
+        If (c_boolCallProcedureForText) Then
+
+            ''Encapsulated 5/7/2019 td
+            ApplyTextToImage(img_LargeLandscape)
+            ApplyMemberPicToImage(img_LargeLandscape)
+
+        Else
+
+            gr = Graphics.FromImage(img_LargeLandscape)
+
+            ''
+            ''Resizing Images in VB.NET
+            ''  https://stackoverflow.com/questions/2144592/resizing-images-in-vb-net
+            ''
+
+            ''gr.DrawString("Drawing text",
+            ''              New Font("Tahoma", 14),
+            ''              New SolidBrush(Color.Green),
+            ''              10, 10)
+
+            gr.DrawString("Person " & txtStudentID.Text,
+                          New Font("Tahoma", labelDefault1.Font.SizeInPoints),
+                          New SolidBrush(labelDefault1.ForeColor),
+                           labelDefault1.Left, labelDefault1.Top)
+
+            gr.DrawString(txtStudentName.Text,
+                          New Font("Tahoma", LabelDefault2.Font.SizeInPoints),
+                          New SolidBrush(LabelDefault2.ForeColor),
+                           LabelDefault2.Left, LabelDefault2.Top)
+
+            gr.Dispose()
+
+        End If ''End of "If (c_boolCallProcedureForText) Then ..... Else ...."
+
+        ''panelLayout.Refresh()
+        ''img.Save("Test.jpg", Imaging.ImageFormat.Png)
+        ''System.Diagnostics.Process.Start("Test.jpg")
+
+        If (False) Then
+            Convert.ToBase64String(System.IO.File.ReadAllBytes("Test.jpg"))
+        End If
+
+        labelDefault1.Visible = True
+        LabelDefault2.Visible = True
+
+        ''Added 5/15/2019 td
+        img_Rotated = CType(img_LargeLandscape.Clone, Image)
+
+        ''5/15 td''Dim bm_source As New Bitmap(img_LargeLandscape)
+        Dim bm_source As New Bitmap(img_Rotated)
+        bm_source.RotateFlip(RotateFlipType.Rotate90FlipNone)
+
+        ''img = ResizeImage(img, pictureboxReview)
+        Dim imgResized As Image
+        imgResized = ResizeImage(bm_source, pictureboxReview)
+
+        pictureboxReview.Image = imgResized
+
+        ''gr.DrawImage()
+
+        If (pboolLargeLandscape) Then
+            pref_image = img_LargeLandscape
+        Else
+            pref_image = imgResized
+        End If
+
+    End Sub ''End of "Private Sub GenerateBuildImage()"
+
+    Private Sub ApplyTextToImage(ByRef par_image As Image)
+        ''
+        ''Added 5/7/2019 td  
+        ''
+        Dim gr As Graphics ''= Graphics.FromImage(img)
+        gr = Graphics.FromImage(par_image)
+
+        ''
+        ''Resizing Images in VB.NET
+        ''  https://stackoverflow.com/questions/2144592/resizing-images-in-vb-net
+        ''
+
+        ''gr.DrawString("Drawing text",
+        ''              New Font("Tahoma", 14),
+        ''              New SolidBrush(Color.Green),
+        ''              10, 10)
+
+        ''Draw white space so that the text can be read more easily. 
+        ApplyWhiteSpaceToImage(par_image, labelDefault1)
+
+        gr.DrawString("Person " & txtStudentID.Text,
+                      New Font("Tahoma", labelDefault1.Font.SizeInPoints),
+                      New SolidBrush(labelDefault1.ForeColor),
+                       labelDefault1.Left, labelDefault1.Top)
+
+        ''Draw white space so that the text can be read more easily. 
+        ApplyWhiteSpaceToImage(par_image, LabelDefault2)
+
+        gr.DrawString(txtStudentName.Text,
+                      New Font("Tahoma", LabelDefault2.Font.SizeInPoints),
+                      New SolidBrush(LabelDefault2.ForeColor),
+                       LabelDefault2.Left, LabelDefault2.Top)
+
+        gr.Dispose()
+
+    End Sub ''End of ""Private Sub ApplyTextToImage(ByRef par_image As Image)
+
+    Private Sub ApplyWhiteSpaceToImage(ByRef par_image As Image, ByVal par_textboxOrLabel As Control)
+        ''
+        ''Added 5/10/2019 td  
+        ''
+        ''    https://docs.microsoft.com/en-us/dotnet/api/system.drawing.graphics.drawimage?view=netframework-4.8
+        ''
+        Dim gr As Graphics ''= Graphics.FromImage(img)
+
+        gr = Graphics.FromImage(par_image)
+
+        With par_textboxOrLabel
+            gr.DrawImage(picturePureWhite.Image, .Left, .Top, .Width, .Height)
+        End With
+
+        gr.Dispose()
+
+    End Sub ''ENd of "Private Sub ApplyWhiteSpaceToImage(ByRef par_image As Image, ByRef par_textboxOrLabel As Control)"
+
+    Private Sub ApplyMemberPicToImage(ByRef par_image As Image)
+        ''
+        ''Added 5/7/2019 td  
+        ''
+        ''
+        ''    https://docs.microsoft.com/en-us/dotnet/api/system.drawing.graphics.drawimage?view=netframework-4.8
+        ''
+        Dim gr As Graphics ''= Graphics.FromImage(img)
+        Dim imgPicture As Image
+        ''Dim imgResized As Image
+
+        ''-----gr = Graphics.FromImage(par_image)
+        imgPicture = PicturePersonLarge.Image
+
+        ''Dim bm_source As New Bitmap(imgPicture)
+        ''imgResized = ResizeImage(bm_source, PicturePersonInLayout)
+
+        gr = Graphics.FromImage(par_image)
+
+        ''
+        ''    https://docs.microsoft.com/en-us/dotnet/api/system.drawing.graphics.drawimage?view=netframework-4.8
+        ''
+        With PicturePersonInLayout
+            gr.DrawImage(imgPicture, .Left, .Top, .Width, .Height)
+        End With
+
+        gr.Dispose()
+
+ExitHandler:
+        ''------gr.Dispose()
+
+    End Sub ''End of ""Private Sub ApplyMemberPicToImage(ByRef par_image As Image)
+
+    Public Shared Function ResizeImage(ByVal InputImage As Image, ByVal parSizingBox As Control) As Image
+        ''
+        ''https://stackoverflow.com/questions/2144592/resizing-images-in-vb-net 
+        ''
+        ''5/7/2019 td''Return New Bitmap(InputImage, New Size(64, 64))
+        Return New Bitmap(InputImage, New Size(parSizingBox.Width, parSizingBox.Height))
+
+    End Function ''Public Shared Function ResizeImage(ByVal InputImage As Image, ByVal parSizingBox As Control) As Image
+
+End Class
