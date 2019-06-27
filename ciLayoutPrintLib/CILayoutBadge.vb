93d2f697f0389f75a37be0e7246acd9a1f7abcff
Thomas Downes <ThomasD@CardIntegrators.com>
2019-06-27 01:45:07 -0700

    Added a class-model to manage multiple labels and a portrait picture.

---
+++ ciLayoutPrintLib/CILayoutBadge.vb
@@ -0,0 +1,20 @@
+﻿Option Explicit On
+Option Strict On
+
+''Added 6/27/2019 thomas downes 
+
+''Imports System.Web.Mvc ''Added 6/27/2019 td
+Imports System.ComponentModel.DataAnnotations ''Added 6/27/2019 td
+
+Public Class CILayoutBadge
+
+    <Key> <Display(Name:="CI Layout ID")>
+    Public Property CILayoutBadgeId() As Integer
+
+    Public FullName As CILayoutText
+    Public HolderID As CILayoutText
+    Public CustomField1 As CILayoutText
+    Public CustomField2 As CILayoutText
+    Public PicPortrait As CILayoutPic
+
+End Class
---
+++ ciLayoutPrintLib/CILayoutPic.vb
@@ -0,0 +1,45 @@
+﻿Option Explicit On
+Option Strict On
+
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+''Imports System.Web.Mvc ''Added 5/28/2019 td
+Imports System.ComponentModel.DataAnnotations ''Added 5/28/2019 td
+Imports System.Drawing ''Added 6/13/2019 thomas d. 
+
+Public Class CILayoutPic
+
+    <Key> <Display(Name:="CI Layout Pic ID")>
+    Public Property CILayoutPicId() As Integer
+
+
+    <Display(Name:="Top-Edge Position in Pixels")>
+    Public Property TopEdgePositionPixels() As Integer
+
+    <Display(Name:="Left-Edge Position in Pixels")>
+    Public Property LeftEdgePositionPixels() As Integer
+
+    <Display(Name:="Width (or Length) in Pixels")>
+    Public Property WidthLengthPixels() As Integer
+
+    <Display(Name:="Height in Pixels")>
+    Public Property HeightPixels() As Integer
+
+    Public Sub New()
+        ''
+        ''Added 6/25/2019 thomas downes
+        ''
+        Me.CILayoutPicId = 1
+
+        Me.LeftEdgePositionPixels = 160
+        Me.TopEdgePositionPixels = 40
+
+        Me.HeightPixels = 200
+        Me.WidthLengthPixels = 160
+
+    End Sub
+
+End Class
+
