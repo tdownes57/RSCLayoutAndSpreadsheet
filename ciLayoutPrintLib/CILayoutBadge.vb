ab50706bc8b0c2dbf3cee8ee32b66e0849432c4d
Thomas Downes <ThomasD@CardIntegrators.com>
2019-06-27 22:27:32 -0700

    I have added procedures to propagate values from  parent to children.

--- ciLayoutPrintLib/CILayoutBadge.vb
+++ ciLayoutPrintLib/CILayoutBadge.vb
@@ -21,6 +21,27 @@ Public Class CILayoutBadge
     Public T1 As New CILayoutText
     Public T2 As New CILayoutText
 
+    Public Property T1_FieldName() As String
+    Public Property T1_TopEdgePositionPixels() As Integer
+    Public Property T1_LeftEdgePositionPixels() As Integer
+    Public Property T1_WidthLengthPixels() As Integer
+    Public Property T1_HeightPixels() As Integer
+    Public Property T1_FontFamilyName() As String
+    Public Property T1_FontSize() As Integer
+    Public Property T1_FontColor() As String
+    Public Property T1_BackgroundColor() As String
+
+    Public Property T2_FieldName() As String
+    Public Property T2_TopEdgePositionPixels() As Integer
+    Public Property T2_LeftEdgePositionPixels() As Integer
+    Public Property T2_WidthLengthPixels() As Integer
+    Public Property T2_HeightPixels() As Integer
+    Public Property T2_FontFamilyName() As String
+    Public Property T2_FontSize() As Integer
+    Public Property T2_FontColor() As String
+    Public Property T2_BackgroundColor() As String
+
+
     Public Sub New()
         ''
         ''Added 6/27/2019 thomas downes
@@ -29,6 +50,38 @@ Public Class CILayoutBadge
         T2.LeftEdgePositionPixels = 2 * T1.LeftEdgePositionPixels
 
     End Sub
+
+    Public Sub Update_T1()
+        ''
+        ''Added 6/27/2019 thomas downes
+        ''
+        T1.BackgroundColor = T1_BackgroundColor
+        T1.FieldName = T1_BackgroundColor
+        T1.FontColor = T1_FontColor
+        T1.FontFamilyName = T1_FontFamilyName
+        T1.FontSize = T1_FontSize
+        T1.HeightPixels = T1_HeightPixels
+        T1.LeftEdgePositionPixels = T1_LeftEdgePositionPixels
+        T1.TopEdgePositionPixels = T1_TopEdgePositionPixels
+        T1.WidthLengthPixels = T1_WidthLengthPixels
+
+    End Sub ''End of "Public Sub Update_T1"
+
+    Public Sub Update_T2()
+        ''
+        ''Added 6/27/2019 thomas downes
+        ''
+        T2.BackgroundColor = T2_BackgroundColor
+        T2.FieldName = T2_BackgroundColor
+        T2.FontColor = T2_FontColor
+        T2.FontFamilyName = T2_FontFamilyName
+        T2.FontSize = T2_FontSize
+        T2.HeightPixels = T2_HeightPixels
+        T2.LeftEdgePositionPixels = T2_LeftEdgePositionPixels
+        T2.TopEdgePositionPixels = T2_TopEdgePositionPixels
+        T2.WidthLengthPixels = T2_WidthLengthPixels
+
+    End Sub ''End of "Public Sub Update_T2"
 
     Public Sub UpdateByFormValues(par_formValues As FormCollection)
         ''
