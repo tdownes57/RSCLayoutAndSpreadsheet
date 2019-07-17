2c90634ef2a014de8d0f10cdfba7f32c8c77e195
CardIntegrators <programming@cardintegrators.com>
2019-07-17 10:51:48 -0700

    Added modules for hard-coding the positions of the graphical-adjustment boxes.

---
+++ ciLayoutDesignVB/modAdjustmentBoxes_V101.vb
@@ -0,0 +1,42 @@
+﻿''
+''Added 7/17/2019 thomas downes 
+''
+Module modAdjustmentBoxes_V101
+    ''
+    ''Added 7/17/2019 thomas downes 
+    ''
+    ''For Images\AdjustWHColor_V01.jpg   
+    ''
+    Public Enum Enum_V101
+        Undetermined
+        WidthIncrease
+        WidthDecrease
+        HeightIncrease
+        HeightDecrease
+        ColorRGB
+    End Enum
+
+    Public EntireGraphic_Width As Integer = 999
+    Public EntireGraphic_Height As Integer = 999
+
+    Public WidthIncrease_Top_ As Integer = 999
+    Public WidthIncrease_Left As Integer = 999
+    Public WidthIncrease_Rght As Integer = 999
+    Public WidthIncrease_Btm_ As Integer = 999
+
+    Public WidthDecrease_Top_ As Integer = 999
+    Public WidthDecrease_Left As Integer = 999
+    Public WidthDecrease_Rght As Integer = 999
+    Public WidthDecrease_Btm_ As Integer = 999
+
+    Public HeightIncrease_Top_ As Integer = 999
+    Public HeightIncrease_Left As Integer = 999
+    Public HeightIncrease_Rght As Integer = 999
+    Public HeightIncrease_Btm_ As Integer = 999
+
+    Public HeightDecrease_Top_ As Integer = 999
+    Public HeightDecrease_Left As Integer = 999
+    Public HeightDecrease_Rght As Integer = 999
+    Public HeightDecrease_Btm_ As Integer = 999
+
+End Module ''Module modAdjustmentBoxes_V101
