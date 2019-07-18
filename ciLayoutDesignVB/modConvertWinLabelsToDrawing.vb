dade61bb0fe8d7157b06339ad3b99a38eb73b910
CardIntegrators <programming@CardIntegrators.com>
2019-07-17 18:51:18 -0700

    Added two conversion modules (yet empty).

---
+++ ciLayoutDesignVB/modConvertWinLabelsToDrawing.vb
@@ -0,0 +1,12 @@
+﻿''
+''Added 7/17/2019 thomas downes 
+''
+Module modConvertWinLabelsToDrawing
+    ''
+    ''Added 7/17/2019 thomas downes 
+    ''
+
+
+
+
+End Module
---
+++ ciLayoutDesignVB/modConvertWinLabelsToWebCSS.vb
@@ -0,0 +1,9 @@
+﻿''
+''Added 7/17/2019 thomas downes 
+''
+Module modConvertWinLabelsToWebCSS
+    ''
+    ''Added 7/17/2019 thomas downes 
+    ''
+
+End Module
