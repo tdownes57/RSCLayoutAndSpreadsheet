d0a32877699261ab0e1b748f081098197b20fe04
Thomas Downes <ThomasD@CardIntegrators.com>
2019-07-18 00:04:15 -0700

    Added a Factory for generating Images which approximate the display of a Label.

---
+++ ciLayoutDesignVB/ClassLabelToImage.vb
@@ -0,0 +1,23 @@
+﻿Option Explicit On ''Added 7/17/2019
+Option Strict On ;'Added 7/17/2019
+
+''
+''Added 7/17/2019
+''
+Imports System.Drawing.Image ''Added 7/17/2019
+
+Public Class ClassLabelToImage
+    ''
+    ''Added 7/17/2019
+    ''
+    Public Function MakeImage(par_label As Label) As Image
+        ''
+        ''Added 7/17/2019 thomas downes
+        ''
+
+
+
+
+    End Function
+
+End Class
