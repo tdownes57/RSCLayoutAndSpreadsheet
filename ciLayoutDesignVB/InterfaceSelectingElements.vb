3085ca7392aafa3fefd476b7986f69621f763359
Thomas Downes <ThomasD@CardIntegrators.com>
2019-08-02 00:17:56 -0700

    Created a partial class for the CtlGraphicFldLabel custom-control class.    This partial class will control the context menu & associated actions.

--- ciLayoutDesignVB/InterfaceSelectingElements.vb
+++ ciLayoutDesignVB/InterfaceSelectingElements.vb
@@ -6,13 +6,14 @@ Public Interface ISelectingElements
     ''
     ''Added 7/31/2019 thomas downes  
     ''
-    Sub LabelsDesignList_Add() ''Implements ISelectingElements.LabelsDesignList_Add
+    Sub LabelsDesignList_Add(par_control As CtlGraphicFldLabel) ''Implements ISelectingElements.LabelsDesignList_Add
 
-    Sub LabelsDesignList_Remove() ''Implements ISelectingElements.LabelsDesignList_Remove
+    Sub LabelsDesignList_Remove(par_control As CtlGraphicFldLabel) ''Implements ISelectingElements.LabelsDesignList_Remove
 
     Function LabelsList_CountItems() As Integer ''Implements ISelectingElements.LabelsDesignList_CountItems
     Function LabelsList_OneOrMoreItems() As Boolean ''Implements ISelectingElements.LabelsDesignList_OneOrMoreItems
     Function LabelsList_TwoOrMoreItems() As Boolean ''Implements ISelectingElements.LabelsDesignList_TwoOrMoreItems
-    Function LabelsList_IsItemIncluded() As Boolean ''Implements ISelectingElements.LabelsDesignList_IsItemIncluded
+    Function LabelsList_IsItemIncluded(par_control As CtlGraphicFldLabel) As Boolean ''Implements ISelectingElements.LabelsDesignList_IsItemIncluded
+    Function LabelsList_IsItemUnselected(par_control As CtlGraphicFldLabel) As Boolean ''Implements ISelectingElements.LabelsDesignList_IsItemIncluded
 
 End Interface ''ENd of "Public Interface ISelectingElements"
