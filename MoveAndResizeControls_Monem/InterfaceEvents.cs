642c779db1c533073d6a81aee25fb822152a0c62
Thomas Downes <thomasd@CardIntegrators.com>
2019-08-03 22:40:03 -0700

    This might work better (moving & resizing Grouped-Elements in unison).

---
+++ MoveAndResizeControls_Monem/InterfaceEvents.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlManager
+{
+    public interface InterfaceEvents
+    {
+        //
+        // Added 8/3/2019 thomas downes. 
+        //
+        //bool AreGroupOfElements;   //Added 8/2/2019 td  
+        //void ActivateGroupMoves(); //Added 8/3/2019 td 
+
+        //public delegate void GroupControlsMoved(int DeltaLeft, int DeltaTop, int DeltaWidth, int DeltaHeight);  //Added 8/2/2019 td
+        //public event GroupControlsMoved GroupMove;  //Added 8/2/2019 td
+
+        void GroupMove(int DeltaLeft, int DeltaTop, int DeltaWidth, int DeltaHeight); 
+
+
+    }
+}
