9dbae6cb8b7d2dc0e9798d526127b38b4a9aa216
CardIntegrators <programming@cardintegrators.com>
2019-08-07 11:36:11 -0700

    Step 3 Take Picture work.

--- ciBadgeDesktop/Views/FormStep3TakePic_VBA.Designer.vb
+++ ciBadgeDesktop/Views/FormStep3TakePic_VBA.Designer.vb
@@ -22,9 +22,32 @@ Partial Class FormStep3TakePic_VBA
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.pictBackStep3_v82 = New System.Windows.Forms.PictureBox()
+        CType(Me.pictBackStep3_v82, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pictBackStep3_v82
+        '
+        Me.pictBackStep3_v82.BackgroundImage = Global.ciBadgeDesktop.My.Resources.Resources.__UI__v82_Step_3a_
+        Me.pictBackStep3_v82.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.pictBackStep3_v82.Location = New System.Drawing.Point(12, 12)
+        Me.pictBackStep3_v82.Name = "pictBackStep3_v82"
+        Me.pictBackStep3_v82.Size = New System.Drawing.Size(987, 663)
+        Me.pictBackStep3_v82.TabIndex = 0
+        Me.pictBackStep3_v82.TabStop = False
+        '
+        'FormStep3TakePic_VBA
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1011, 687)
+        Me.Controls.Add(Me.pictBackStep3_v82)
+        Me.Name = "FormStep3TakePic_VBA"
         Me.Text = "FormStep3TakePic"
+        CType(Me.pictBackStep3_v82, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents pictBackStep3_v82 As PictureBox
 End Class
