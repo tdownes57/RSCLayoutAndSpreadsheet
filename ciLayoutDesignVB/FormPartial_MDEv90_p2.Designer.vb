86c607f06c763330b1f16822ad68c6dcf4effbbe
CardIntegrators <programming@cardintegrators.com>
2019-09-09 13:29:39 -0700

    The project builds!!

--- ciLayoutDesignVB/FormPartial_MDEv90_p2.Designer.vb
+++ ciLayoutDesignVB/FormPartial_MDEv90_p2.Designer.vb
@@ -1,37 +1,30 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class FormPartial_MDEv90
+﻿Partial Class FormMainEntry_v90
     Inherits System.Windows.Forms.Form
 
-    'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
-    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-        Try
-            If disposing AndAlso components IsNot Nothing Then
-                components.Dispose()
-            End If
-        Finally
-            MyBase.Dispose(disposing)
-        End Try
-    End Sub
+    ''Form overrides dispose to clean up the component list.
+    ''<System.Diagnostics.DebuggerNonUserCode()>
+    ''Protected Overrides Sub Dispose_N(ByVal disposing As Boolean)
+    ''    Try
+    ''        If disposing AndAlso components IsNot Nothing Then
+    ''            components.Dispose()
+    ''        End If
+    ''    Finally
+    ''        MyBase.Dispose(disposing)
+    ''    End Try
+    ''End Sub
 
     'Required by the Windows Form Designer
-    Private components As System.ComponentModel.IContainer
+    Private components_p2 As System.ComponentModel.IContainer
 
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
-    Private Sub InitializeComponent()
-        Me.SuspendLayout()
-        '
-        'FormPartial_MDEv90
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1253, 636)
-        Me.Name = "FormPartial_MDEv90"
-        Me.Text = "FormPartial_MDEv90"
-        Me.ResumeLayout(False)
+    ''<System.Diagnostics.DebuggerStepThrough()>
+    ''Private Sub InitializeComponent()
+    ''    components = New System.ComponentModel.Container
+    ''    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    ''    Me.ClientSize = New System.Drawing.Size(800, 450)
+    ''    Me.Text = "Partial_ProtoTwo"
+    ''End Sub
 
-    End Sub
 End Class
