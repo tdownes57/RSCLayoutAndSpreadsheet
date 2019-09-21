58bc6ed7d00d870f4b6b10bd5f6941313d61ab72
Thomas Downes <ThomasD@CardIntegrators.com>
2019-09-21 09:29:59 -0700

    Added a test project called RotateTextTest.

---
+++ RotateTextTest/ClassMethods.vb
@@ -0,0 +1,28 @@
+﻿Option Explicit On
+Option Strict On
+''
+''Added 9/21/2019 td
+''
+Imports System.Reflection.Assembly
+
+Public Class ClassMethods
+    ''
+    ''Added 9/21/2019 td
+    ''
+    Public Sub Add_Text_Image(par_form As Form)
+
+
+
+
+    End Sub
+
+    Public Sub Create_a_Rotated_Image()
+
+
+
+    End Sub
+
+
+
+
+End Class
---
+++ RotateTextTest/FormRotateText.vb
@@ -0,0 +1,44 @@
+﻿Option Explicit On
+Option Strict On
+''
+''Added 9/21/2019 thomas d
+''
+Imports System.Reflection.Assembly
+
+Public Class FormRotateText
+    Private Sub FormRotateText_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ''Dim objInfo As Type ''System.Reflection.Assembly
+        Dim each_member As Reflection.MemberInfo
+
+        Dim objClass1 As New ClassMethods
+        Dim strMethodName As String
+        Dim strMethodWithSpaces As String
+
+        ''objInfo = (TypeOf objClass1)
+
+        ''// Using Reflection to get information of an Assembly  
+        ''System.Reflection.Assembly info = TypeOf (System.Int32).Assembly;
+
+        Dim t As Type = objClass1.GetType
+
+        For Each each_member In t.GetMembers()
+
+            strMethodName = each_member.Name
+
+            strMethodWithSpaces = strMethodName.Replace("_", " ")
+
+            Dim each_link As New LinkLabel
+
+            each_link.Visible = True
+            each_link.Text = strMethodWithSpaces
+
+            AddHandler each_link.LinkClicked, AddressOf each_member
+
+
+
+        Next each_member
+
+
+    End Sub
+End Class
