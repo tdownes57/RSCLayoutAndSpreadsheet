acd51b0b9388926dbd326367bfc2b97107255120
Thomas Downes <ThomasD@CardIntegrators.com>
2019-10-01 01:28:52 -0700

    Added a new project ciBadgeRecipients.

---
+++ ciBadgeRecipients/Recipient.vb
@@ -0,0 +1,27 @@
+﻿Option Explicit On
+Option Strict On
+Option Infer Off
+''
+''Added 10/1/2019 td  
+''
+Imports ciBadgeInterfaces
+
+Public Class Recipient
+    Implements IRecipient
+    ''
+    ''Added 10/1/2019 td  
+    ''
+    Public Property ID_Guid As System.Guid
+
+    Public Property Customer_Guid As System.Guid
+
+    Public Property Personality_Guid As System.Guid
+
+
+
+
+
+
+
+
+End Class
