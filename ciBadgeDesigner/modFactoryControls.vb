92d4b6e9a3fbde9891fcd6fca98444f5fe2a3047
Thomas Downes <ThomasD@CardIntegrators.com>
2019-10-02 02:02:49 -0700

    Added a factory for different types of Element-Field Controls, for the situation (badge layout, data entry).

---
+++ ciBadgeDesigner/modFactoryControls.vb
@@ -0,0 +1,36 @@
+﻿''
+''Added 10/2/2019 thomas downes
+''
+
+Module modFactoryControls
+
+    Public Enum EnumControlsMode
+
+        Undetermined
+
+        BadgeCard_Layout
+        BadgeCard_Preview
+
+        DataEntry_Support
+        DataEntry_Design
+
+    End Enum
+
+    ''Public Function GetFieldControl_Generic(par_enum As EnumControlsMode) As ICtlElementFieldCtl
+
+
+    ''Select Case par_enum
+
+    ''    Case EnumControlsMode.BadgeCard_Layout
+
+    ''              Return New CtLGraphicFieldCtl 
+
+
+    ''  End Select  
+
+    ''End Function
+
+
+
+
+End Module
---
+++ ciBadgeInterfaces/InterfaceCtlElementField.vb
@@ -0,0 +1,16 @@
+﻿''
+''Added 10/2/2019 td
+''
+Imports cbib
+
+Public Interface ICtlElementFieldCtl
+    ''
+    ''Added 10/2/2019 td
+    ''
+    ''Probably a circular reference !!  ----Property ElementField As classel
+
+
+
+
+
+End Interface
