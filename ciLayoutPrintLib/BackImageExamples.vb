eb90ae8e490e2364b9fb29b7c9e2d8ffe0b332a9
Thomas Downes <thomasd@CardIntegrators.com>
2019-10-05 00:35:55 -0700

    Renamed an images folder to "BackExamples".

--- ciLayoutPrintLib/BackImageExamples.vb
+++ ciLayoutPrintLib/BackImageExamples.vb
@@ -121,7 +121,7 @@ ExitHandler:
         ''8/21/2019 td''strPathToFolderWithPics = My.Application.Info.DirectoryPath & "\BackgroundEgs"
 
         ''8/21/2019 td''strPathToFolderWithPics = My.Application.Info.DirectoryPath & "\BackgroundEgs"
-        strPathToFolderWithPics = PathToFolderWithBacks.Replace("\\", "\") & "\BackgroundEgs"
+        strPathToFolderWithPics = PathToFolderWithBacks.Replace("\\", "\") ''10/5/2019 td'' & "\BackgroundEgs"
 
         all_Jpegs = (New System.IO.DirectoryInfo(strPathToFolderWithPics)).EnumerateFiles()
 
