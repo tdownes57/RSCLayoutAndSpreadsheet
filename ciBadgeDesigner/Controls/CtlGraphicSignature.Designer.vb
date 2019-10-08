fb4da866a9dee9270ab1112753410442a501f41a
CardIntegrators <programming@cardintegrators.com>
2019-10-08 14:18:32 -0700

    Fixed CtlGraphicSignature.

--- ciBadgeDesigner/Controls/CtlGraphicSignature.Designer.vb
+++ ciBadgeDesigner/Controls/CtlGraphicSignature.Designer.vb
@@ -1,8 +1,8 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class CtlGraphicSignature
-    Inherits System.Windows.Forms.Control
+    Inherits System.Windows.Forms.UserControl
 
-    'Control overrides dispose to clean up the component list.
+    'UserControl overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()> _
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
@@ -14,16 +14,16 @@ Partial Class CtlGraphicSignature
         End Try
     End Sub
 
-    'Required by the Control Designer
+    'Required by the Windows Form Designer
     Private components As System.ComponentModel.IContainer
 
-    ' NOTE: The following procedure is required by the Component Designer
-    ' It can be modified using the Component Designer.  Do not modify it
-    ' using the code editor.
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         components = New System.ComponentModel.Container()
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
     End Sub
 
 End Class
-
--- ciBadgeDesigner/Controls/CtlGraphicSignature.vb
+++ ciBadgeDesigner/Controls/CtlGraphicSignature.vb
@@ -1,9 +1,3 @@
 ﻿Public Class CtlGraphicSignature
 
-    Protected Overrides Sub OnPaint(ByVal e As System.Windows.Forms.PaintEventArgs)
-        MyBase.OnPaint(e)
-
-        'Add your custom paint code here
-    End Sub
-
 End Class
