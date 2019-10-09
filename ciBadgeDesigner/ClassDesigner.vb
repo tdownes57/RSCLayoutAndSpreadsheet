d39648ee65648734e9af96ade8671899d5d74992
CardIntegrators <programming@cardintegrators.com>
2019-10-09 15:09:08 -0700

    Renamed a class to ClassGroupMoveEvents (from ClassGroupMove).

--- ciBadgeDesigner/ClassDesigner.vb
+++ ciBadgeDesigner/ClassDesigner.vb
@@ -54,8 +54,8 @@ Public Class ClassDesigner
     '' #2 8-3-2019 td''Private WithEvents mod_moveAndResizeCtls As New MoveAndResizeControls_Monem.ControlMove_GroupMove ''Added 8/3/2019 td  
     ''#1 10/1/2019 td''Private WithEvents mod_groupedMove As New ClassGroupMove(Me) ''8/4/2019 td''New ClassGroupMove
     '' #2 10/1/2019 td''Private WithEvents mod_groupedMove As New ClassGroupMove(Me.LayoutFunctions) ''8/4/2019 td''New ClassGroupMove
-    Private WithEvents mod_groupedMove As New ClassGroupMove(Me) ''8/4/2019 td''New ClassGroupMove
-    Private WithEvents mod_sizingPic_events As New ClassGroupMove(Me) ''Added 10/9/2019 td  
+    Private WithEvents mod_groupedMove As New ClassGroupMoveEvents(Me) ''8/4/2019 td''New ClassGroupMove
+    Private WithEvents mod_sizingPic_events As New ClassGroupMoveEvents(Me) ''Added 10/9/2019 td  
 
     Private Const mc_boolAllowGroupMovements As Boolean = True ''False ''True ''False ''Added 8/3/2019 td  
     Private Const mc_boolBreakpoints As Boolean = True
