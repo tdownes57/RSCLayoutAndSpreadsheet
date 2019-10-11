ff14504c538455c1e336c121e8fe11d03ea2099d
CardIntegrators <programming@cardintegrators.com>
2019-10-11 14:53:15 -0700

    Added higher-level caches, e.g. Badge Layout (with 2 sides if double-sided), and Personality Config (which can have > 1 Layout, for better or worse).

---
+++ ciBadgeCustomer/BadgeLayoutCache.vb
@@ -0,0 +1,20 @@
+﻿
+''
+''Added 10/11/2019 thomas downes
+''
+Imports ciBadgeElements
+
+Public Class BadgeLayoutCache
+    ''
+    ''Added 10/11/2019 thomas downes
+    ''
+    Public Property LayoutId_GUID As System.Guid ''Added 10/11/2019 td 
+
+    Public Property LayoutName As String ''Added 10/11/2019 td 
+
+    Public Property DoubleSided As Boolean ''Added 10/11/2019 td
+
+    Public Property Side1orFirst_Cache As ClassElementsCache ''The Front side. ----Added 10/11/2019 td 
+    Public Property Side2orSecond_Cache As ClassElementsCache ''The Back side of the badge, if applicable. ----Added 10/11/2019 td 
+
+End Class ''ENd of "Public Class BadgeLayoutCache"
---
+++ ciBadgeCustomer/PersonalityCache.vb
@@ -0,0 +1,29 @@
+﻿''
+''Added 10/11/2019 td  
+''
+Imports ciBadgeFields ''Added 10/11/2019 td
+Imports ciBadgeElements ''Added 10/11/2019 td
+
+<Serializable>
+Public Class PersonalityCache
+    ''
+    ''Added 10/11/2019 td  
+    ''
+    Public Property ConfigId_GUID As System.Guid ''Added 10/11/2019 td
+    Public Property ConfigId_Int As Integer ''Added 10/11/2019 td 
+
+    Public Property PathToXml_Saved As String ''Added 10/11/2019 td
+
+    Public Property ListOfFields As List(Of ClassFieldAny) ''Added 10/11/2019 td
+
+    Public Property ElementFields_DataEntry_V8 As List(Of ClassElementField)
+    Public Property ElementFields_DataEntry_V9 As List(Of ClassElementField)
+
+    Public Property ListOfLayouts As List(Of BadgeLayoutCache) ''Added 10/11/2019 td
+
+
+
+
+
+
+End Class ''End of "Public Class ClassPersonalityCache"
