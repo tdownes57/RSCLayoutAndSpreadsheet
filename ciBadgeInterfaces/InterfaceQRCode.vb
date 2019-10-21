4b2ac3158170a892c977aec2a1ed756c3490a676
CardIntegrators <programming@cardintegrators.com>
2019-10-21 12:09:50 -0700

    Added properties to the InterfaceQRCode.

--- ciBadgeInterfaces/InterfaceQRCode.vb
+++ ciBadgeInterfaces/InterfaceQRCode.vb
@@ -6,7 +6,23 @@ Public Interface InterfaceQRCode
     ''
     ''Added 10/21/2019 td
     ''
+    '**See ** above.**objPMgr.Cfg = gCurCfg.lCfgID
+    '**See ** above.**objPMgr.CfgFile = GlobalCfg.sCfgFile
+    '**See ** above.**objPMgr.PrintDataFile = GlobalCfg.sLayoutFile
+    '**See ** above.**objPMgr.QRCodesEnabled = gCurCfg.bEnableQRCodes
+    '**See ** above.**objPMgr.QRCodesFolder = GlobalCfg.sTempFilesFolder '' C:\CI Solutions\CI Badge\Data\TempFiles  ... Has QR01_12346.jpg, for example.
+    '**See ** above.**objPMgr.PrinterName = strPrinter
 
+    Property Cfg As Integer
 
+    Property CfgFile As String
 
-End Interface
+    Property PrintDataFile As String
+
+    Property QRCodesEnabled As Boolean
+
+    Property QRCodesFolder As String
+
+    Property PrinterName As String
+
+End Interface ''ENd of "Public Interface InterfaceQRCode"
