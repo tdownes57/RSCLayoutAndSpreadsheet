fcfa04667b63ff699c5135273f15d52d72a58449
tdownes57 <tomdownes1@gmail.com>
2021-08-20 20:18:46 -0700

    Added new project, abcBundleBadgeImagesToEmail_Core.

---
+++ abcBundleBadgeImagesToEmail/EmailFunctionality.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//
+// Add 8/20/2021 Thomas Downes
+//
+//    This is for emailing a ZIP (or compressed folder?) 
+//    of Badge images/JPEGs. 
+//
+//
+namespace abcBundleBadgeImagesToEmail
+{
+    public class EmailFunctionality
+    {
+        //
+        // Add 8/20/2021 Thomas Downes
+        //
+        //    This is for emailing a ZIP (or compressed folder?) 
+        //    of Badge images/JPEGs. 
+        //
+        //
+    }
+}
