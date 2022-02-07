e46f61738644e7524dda2acf47edb613c4821a51
tdownes57 <tomdownes1@gmail.com>
2022-02-07 03:15:36 -0800

    New project EPS_VectorImagingByAspose

---
+++ EPS_ImagingByAspose/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPS_ImagingByAspose
+{
+    public class Class1
+    {
+    }
+}
