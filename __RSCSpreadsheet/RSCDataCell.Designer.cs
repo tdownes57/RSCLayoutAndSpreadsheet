dba0ee243f504f3e28938cf33e4a23bff96d3f6a
tdownes57 <tomdownes1@gmail.com>
2023-11-14 10:26:48 -0800

    New project __RSCSpreadsheet Five (5) usercontrols, one big control (RSCSpreadsheet)

--- __RSCSpreadsheet/RSCDataCell.Designer.cs
+++ __RSCSpreadsheet/RSCDataCell.Designer.cs
@@ -1,13 +1,13 @@
 ﻿namespace __RSCSpreadsheet
 {
-    partial class UserControl1
+    partial class RSCDataCell
     {
-        /// <summary>
+        /// <summary> 
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+        /// <summary> 
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -22,7 +22,7 @@
 
         #region Component Designer generated code
 
-        /// <summary>
+        /// <summary> 
         /// Required method for Designer support - do not modify 
         /// the contents of this method with the code editor.
         /// </summary>
@@ -30,7 +30,6 @@
         {
             components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
         }
 
         #endregion
