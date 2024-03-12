a45b848f877b8fd99bdf4f880da2a77c1b7c91d3
Thomas Downes <tomdownes1@gmail.com>
2024-03-12 12:35:55 -0700

    Now,     public interface IDoublyLinkedItem is a base-interface for     public interface IDoublyLinkedItem<TypeOfItem> : IDoublyLinkedItem

--- RSCLibraryDLLOperations/InterfaceDoublyLinkedItem.cs
+++ RSCLibraryDLLOperations/InterfaceDoublyLinkedItem.cs
@@ -8,7 +8,62 @@
 namespace RSCLibraryDLLOperations
 {
 
-    public interface IDoublyLinkedItem<TypeOfItem>
+    public interface IDoublyLinkedItem<TypeOfItem> : IDoublyLinkedItem
+    {
+        //-----------------------------------------------------------------------------
+        //-----------------------------------------------------------------------------
+        //  The following are generic-type methods. 
+        //
+        //    (I am moving these to generically-type public interface IDoublyLinkedItem<TypeOfItem>,
+        //    versus public interface IDoublyLinkedItem.)
+        //
+        //    For the NON-generically-typed methods, see the following, declared below this
+        //      current interface.
+        //
+        //               public interface IDoublyLinkedItem
+        //
+        //    As you can see, this generically-typed interface derives from the non-generic
+        //       interface.  (See "...  : IDoublyLinkedItem" above).
+        //
+        //    ---Thomas C. Downes, 3/12/2024
+        //
+        //-----------------------------------------------------------------------------
+        //-----------------------------------------------------------------------------
+
+        IDoublyLinkedItem<TypeOfItem> DLL_GetItemNext();
+        IDoublyLinkedItem<TypeOfItem> DLL_GetItemNext(int param_iterationsOfNext);
+
+        IDoublyLinkedItem<TypeOfItem> DLL_GetItemPrior();
+        IDoublyLinkedItem<TypeOfItem> DLL_GetItemPrior(int param_iterationsOfPrior);
+
+        /// <summary>
+        /// Gets the underlying control.
+        /// </summary>
+        /// <returns></returns>
+        TypeOfItem DLL_UnboxControl();
+        //Having trouble here. See "using" above. 3/2024  Control DLL_UnboxControl();
+
+        void DLL_SetItemNext(IDoublyLinkedItem<TypeOfItem> param);
+        void DLL_SetItemPrior(IDoublyLinkedItem<TypeOfItem> param);
+
+        // Added 12/30/2023 
+        // ---DIFFICULT AND CONFUSING---
+        // By CS-related rules of iteration, by moving ahead
+        // a number of jumps equal to the item-count of the range,
+        // we get the first post-range item.
+        // ---12/30/2023
+        /// <summary>
+        /// Get item following a range (if the implicit parameter is the first item in a range). Sometimes we need the Item which follows the Range, to prepare for a possible Undo.
+        /// </summary>
+        /// <param name="param_rangeSize">This is the item-count of the range, or size of the range.</param>
+        /// <returns>The first item which follows the range.</returns>
+        IDoublyLinkedItem<TypeOfItem> DLL_GetNextItemFollowingRange(int param_rangeSize, bool param_mayBeNull);
+
+    }
+
+
+
+    public interface IDoublyLinkedItem
     {
         // Added 11/02/2023 Thomas Downes
         bool Selected { get; set; }
@@ -34,21 +89,6 @@ namespace RSCLibraryDLLOperations
         /// <returns>Returns a True or False.</returns>
         bool DLL_HasPrior();
 
-        IDoublyLinkedItem<TypeOfItem> DLL_GetItemNext();
-        IDoublyLinkedItem<TypeOfItem> DLL_GetItemNext(int param_iterationsOfNext);
-
-        IDoublyLinkedItem<TypeOfItem> DLL_GetItemPrior();
-        IDoublyLinkedItem<TypeOfItem> DLL_GetItemPrior(int param_iterationsOfPrior);
-
-        /// <summary>
-        /// Gets the underlying control.
-        /// </summary>
-        /// <returns></returns>
-        TypeOfItem DLL_UnboxControl();
-        //Having trouble here. See "using" above. 3/2024  Control DLL_UnboxControl();
-
-        void DLL_SetItemNext(IDoublyLinkedItem<TypeOfItem> param);
-        void DLL_SetItemPrior(IDoublyLinkedItem<TypeOfItem> param);
 
         // Whenever a Row or Column is deleted, and saved into a DLL Operation,
         // the outer edges ---MUST BE CLEANED--- of obselete references.
@@ -62,19 +102,6 @@ namespace RSCLibraryDLLOperations
         void DLL_ClearReferencePrior(char par_typeOp);
         void DLL_ClearReferenceNext(char par_typeOp);
 
-        // Added 12/30/2023 
-        // ---DIFFICULT AND CONFUSING---
-        // By CS-related rules of iteration, by moving ahead
-        // a number of jumps equal to the item-count of the range,
-        // we get the first post-range item.
-        // ---12/30/2023
-        /// <summary>
-        /// Get item following a range (if the implicit parameter is the first item in a range). Sometimes we need the Item which follows the Range, to prepare for a possible Undo.
-        /// </summary>
-        /// <param name="param_rangeSize">This is the item-count of the range, or size of the range.</param>
-        /// <returns>The first item which follows the range.</returns>
-        IDoublyLinkedItem<TypeOfItem> DLL_GetNextItemFollowingRange(int param_rangeSize, bool param_mayBeNull);
-
         // Added 1/4/2024 td
         string DLL_GetValue(); // Added 1/4/2024 td
 
@@ -87,9 +114,65 @@ namespace RSCLibraryDLLOperations
         int DLL_CountItemsAllInList();
         int DLL_CountItemsPrior();
         //1/13/2024 int DLL_CountItemsNext();
+
+        //-----------------------------------------------------------------------------
+        //-----------------------------------------------------------------------------
+        //  The following are commented out, because they reference a generic type. 
+        //
+        //    (I am moving these to generically-type public interface IDoublyLinkedItem<TypeOfItem>,
+        //    versus public interface IDoublyLinkedItem.)
+        //
+        //    For these commented methods, See the following, declared above.
+        //
+        //               public interface IDoublyLinkedItem<TypeOfItem>
+        //
+        //    ---Thomas C. Downes, 3/12/2024
+        //
+        //-----------------------------------------------------------------------------
+        //-----------------------------------------------------------------------------
+
+        //IDoublyLinkedItem<TypeOfItem> DLL_GetItemNext();
+        //IDoublyLinkedItem<TypeOfItem> DLL_GetItemNext(int param_iterationsOfNext);
+
+        //IDoublyLinkedItem<TypeOfItem> DLL_GetItemPrior();
+        //IDoublyLinkedItem<TypeOfItem> DLL_GetItemPrior(int param_iterationsOfPrior);
+
+        ///// <summary>
+        ///// Gets the underlying control.
+        ///// </summary>
+        ///// <returns></returns>
+        //TypeOfItem DLL_UnboxControl();
+        ////Having trouble here. See "using" above. 3/2024  Control DLL_UnboxControl();
+
+        //void DLL_SetItemNext(IDoublyLinkedItem<TypeOfItem> param);
+        //void DLL_SetItemPrior(IDoublyLinkedItem<TypeOfItem> param);
+
+        // Added 12/30/2023 
+        // ---DIFFICULT AND CONFUSING---
+        // By CS-related rules of iteration, by moving ahead
+        // a number of jumps equal to the item-count of the range,
+        // we get the first post-range item.
+        // ---12/30/2023
+        /// <summary>
+        /// Get item following a range (if the implicit parameter is the first item in a range). Sometimes we need the Item which follows the Range, to prepare for a possible Undo.
+        /// </summary>
+        /// <param name="param_rangeSize">This is the item-count of the range, or size of the range.</param>
+        /// <returns>The first item which follows the range.</returns>
+        //IDoublyLinkedItem<TypeOfItem> DLL_GetNextItemFollowingRange(int param_rangeSize, bool param_mayBeNull);
+
     }
 
 
+    public interface IDoublyLinkedItem_NOTINUSE
+    {
+        //
+        // Added 3/12/2024 Thomas Downes
+        //
+
+
+
+
+    }
 
 
 }
