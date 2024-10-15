2def651470c9f6b14956043d414cd0fe30e6a4d3
Thomas Downes <tomdownes1@gmail.com>
2024-10-14 17:38:04 -0700

    Moved RSCLibraryDLLOperations, to the folder called Core Libraries.

--- TestingRSCSpreadsheet_VB/FormSimpleDemoOfCSharp1D.vb
+++ TestingRSCSpreadsheet_VB/FormSimpleDemoOfCSharp1D.vb
@@ -1,3 +1,21 @@
-﻿Public Class FormSimpleDemoOfCSharp1D
+﻿''
+'' Added 10/14/2024 T_homas C. D_ownes 
+''
+Imports RSCLibraryDLLOperations
+
+Public Class FormSimpleDemoOfCSharp1D
+    ''
+    '' Added 10/14/2024 thomas c. downes 
+    ''
+    Private m_list As DLLList(Of TwoCharacterDLLItem)
+
+
+
+
+
+
+
+
+
 
 End Class
